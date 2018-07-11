f4f9bcf34e6a4061a6d2c7d6bd50c110c98993c4
Kael Zhang <i+github@kael.me>
2018-07-11 17:51:22 +0800

    WIP

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-exports.Payment = require('./lib/payment').Payment;
-exports.middleware = require('./lib/middleware');
+const {
+  Payment,
+  xmlParse,
+  xmlStringify,
+  sign
+} = require('./lib/payment')
+
+module.exports = {
+  Payment,
+  xmlParse,
+  xmlStringify,
+  sign,
+  fail: return_msg => xmlStringify({
+    return_code: 'FAIL',
+    return_msg
+  }),
+  success: () => xmlStringify({
+    return_code: 'SUCCESS'
+  })
+}
